25108200fa2fec0e136132a7c9a65d1fa8031852
Yichende <957137116@qq.com>
2023-09-18 17:30:43 +0800

    add reLoad and DOM BUG caused v-if

--- vue-demo/src/components/WaterFall.vue
+++ vue-demo/src/components/WaterFall.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container">
+  <!-- BUG: v-if 导致 DOM 渲染失效 -->
+  <div class="container" v-if="isReloadAlive">
     <div v-for="(item, i) in imgArr" ref="boxRef" class="box">
       <div class="box-img" ref="boxImgRef">
         <img :src="item" :key="i" alt="" />
       </div>
     </div>
   </div>
-  <button @click="test" style="width: 200px; height: 50px;">test</button>
+  <!-- <button @click="test" style="width: 200px; height: 50px;">test</button> -->
 </template>
 
 <script setup lang=ts>
+import { nextTick } from 'process';
 import { onMounted, onBeforeMount, ref } from 'vue';
 
 // 获取图片
@@ -28,25 +30,40 @@ const debounce = function (func: Function, delay = 1000) {
   };
 }
 
+// 刷新
+const isReloadAlive = ref(true);
+
+function reload() {
+  isReloadAlive.value = false;
+  nextTick(() => {
+    isReloadAlive.value = true;
+    console.log("触发了reload");
+  })
+}
+
+
+
 // 存储 box 容器Dom
 const boxRef = ref<HTMLElement[]>([]);
 
-
 // 视窗宽度
 const winWidth = ref(window.innerWidth);
+
 const getWinWidth = () => {
   winWidth.value = window.innerWidth;
-  console.log("windowWidth", winWidth.value);
-  // 重新获取一行图片数量
-  getImgNum();
+  console.log("testWindowWidth", winWidth.value);
 }
 // 重新获取视窗宽度
 const reGetWinWidth = debounce(getWinWidth, 1000);
 
+// 图片宽度
+const imgWidth = ref(0);
+
 const getImgWidth = () => {
-  const boxImg = document.querySelector(".box-img") as HTMLElement;
+  const boxImg = document.querySelector(".box-img") as HTMLElement; //BUG
   if (boxImg) {
-    return boxImg.offsetWidth;
+    imgWidth.value = boxImg.offsetWidth;
+    console.log("testBoxImgOffsetW", boxImg.offsetWidth);
   } else {
     return 0;
   }
@@ -54,57 +71,73 @@ const getImgWidth = () => {
 
 // 一行的图片数量
 let imgNum = ref(0);
-// 图片宽度
-let imgWidth = ref(0);
 // 计算一行图片数量
 const getImgNum = () => {
-  imgWidth.value = getImgWidth();
   imgNum.value = imgWidth.value === 0 ? 0 : Math.floor(winWidth.value / imgWidth.value);
+  console.log("testImgWidth", imgWidth.value);
+  // console.log("testImgNum", imgNum.value);
 }
-onBeforeMount(() => {
-  // 将图片放入数组
-  imgMap.forEach((val = '', key) => {
-    imgArr.value.push(key);
-  });
-});
-onMounted(() => {
-
-  // 获取初始图片宽度
-  getWinWidth();
-  // 监听窗口宽度变化
-  window.addEventListener('resize', () => {
-    reGetWinWidth();
-    waterFall();
-  });
-
-});
 
 const waterFall = () => {
   const BoxHeightArr = [];
   const boxArr = boxRef.value;
+  // console.log("testBoxArr", boxArr);
   for (let i = 0; i < boxArr.length; i++) {
     if (i < imgNum.value) {
       // 第一行各个图片的高度
       BoxHeightArr.push(boxArr[i].offsetHeight);
+      console.log("testBoxArrLength",boxArr.length)
+      console.log("test i<imgNum:", i);
+      console.log("testBoxHeightArr", BoxHeightArr[i]);
+      console.log(" ");
     } else {
       let minHeight = Math.min.apply(null, BoxHeightArr);
+      console.log("testMinHeight", minHeight);
       // 返回数组下标
       let minIndex = BoxHeightArr.indexOf(minHeight);
       boxArr[i].style.position = "absolute";
       boxArr[i].style.top = minHeight + "px";
-      boxArr[i].style.left = (imgWidth.value) * minIndex + 'px'; //BUG
-      console.log("i: ", i + ", top: ", boxArr[i].style.top + ", left: ",boxArr[i].style.left);
+      boxArr[i].style.left = (imgWidth.value) * minIndex + 'px';
+      console.log("i: ", i + ", top: ", boxArr[i].style.top + ", left: ", boxArr[i].style.left);
       console.log("minHeigth: ", minHeight + ", minIndex: ", minIndex);
       console.log(" ");
       // 更新最短一列的高度
       BoxHeightArr[minIndex] += boxArr[i].offsetHeight;
     }
   }
-  console.log(imgWidth.value);
+  console.log("imgWidth", imgWidth.value);
+  console.log("imgNum", imgNum.value);
+  reload();
 }
+
+
+const reWaterFall = debounce(waterFall, 1200);
+
 window.onload = function () {
-  waterFall();
+  // 监听窗口宽度变化
+  window.addEventListener('resize', () => {
+    reGetWinWidth();
+    reWaterFall();
+  });
 }
+
+onBeforeMount(() => {
+  // 将图片文件放入数组
+  imgMap.forEach((val = '', key) => {
+    imgArr.value.push(key);
+  });
+});
+onMounted(() => {
+  // 获取视窗宽度
+  getWinWidth();
+  // 获取图片宽度
+  getImgWidth();
+  // 获取第一行图片数量
+  getImgNum();
+
+  waterFall();
+});
+
 const test = () => {
   // for (const i of boxImgRef.value) {
   //   console.log(i.offsetHeight);
@@ -124,7 +157,7 @@ const test = () => {
 .container {
   position: relative;
   /* TODO */
-  /* overflow: hidden;  */
+  overflow: hidden;
 }
 
 .box {
